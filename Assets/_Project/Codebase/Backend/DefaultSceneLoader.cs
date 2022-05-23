8142f7f34aad1e1c6356d4a187d75864ba479cec
Danon5 <danonfive@gmail.com>
2022-05-23 02:52:33 -0700

    - imported textmesh pro - imported custom font - added menu scenes

---
+++ Assets/_Project/Codebase/Backend/DefaultSceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using PixelSim.SceneLoading;
+using UnityEngine;
+
+namespace PixelSim.Backend
+{
+    public sealed class DefaultSceneLoader : MonoBehaviour
+    {
+        private async void Awake()
+        {
+            Task[] sceneLoadTasks = new Task[2];
+            
+            sceneLoadTasks[0] = SceneLoader.LoadFrontendScene(FrontendScenes.MAIN_MENU_BACKGROUND);
+            sceneLoadTasks[1] = SceneLoader.LoadFrontendScene(FrontendScenes.MAIN_MENU_UI);
+
+            await Task.WhenAll(sceneLoadTasks);
+        }
+    }
+}
--- Assets/_Project/Codebase/Backend/Engine.cs
+++ Assets/_Project/Codebase/Backend/Engine.cs
@@ -1,21 +1,12 @@
-﻿using System.Threading.Tasks;
-using PixelSim.SceneLoading;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace PixelSim.Backend
 {
     public sealed class Engine : MonoBehaviour
     {
-        private async void Awake()
+        private void Awake()
         {
             Application.targetFrameRate = 60;
-
-            Task[] tasks = new Task[2];
-            
-            tasks[0] = SceneLoader.LoadFrontendScene(FrontendScenes.MAIN_MENU_BACKGROUND);
-            tasks[1] = SceneLoader.LoadFrontendScene(FrontendScenes.MAIN_MENU_UI);
-
-            await Task.WhenAll(tasks);
         }
     }
 }
